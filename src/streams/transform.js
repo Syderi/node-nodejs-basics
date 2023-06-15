4cbfea14a065366bcf39cd555d52188dc9c90f85
Syderi <syderi@yahoo.com>
2023-06-15 23:31:18 +0300

    feat: transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
-const transform = async () => {
-    // Write your code here 
+import { Transform } from 'stream';
+
+const transform = () => {
+  const reverseTransform = new Transform({
+    transform(chunk, _, callback) {
+      const reversedChunk =
+        chunk.toString().split('').reverse().join('') + '\n';
+      this.push(reversedChunk);
+      callback();
+    },
+  });
+
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
-await transform();
+transform();
