9ef2fe1bbe9caa797fdf3f20650539c26286a28c
Syderi <syderi@yahoo.com>
2023-06-15 13:04:05 +0300

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
-const create = async () => {
-    // Write your code here 
-};
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-await create();
+async function createFile() {
+  const currentFilePath = fileURLToPath(import.meta.url);
+  const currentDirPath = path.dirname(currentFilePath);
+  const filePath = path.join(currentDirPath, 'files', 'fresh.txt');
+  const fileContent = 'I am fresh and young';
+
+  try {
+    await fs.access(filePath);
+    throw new Error('FS operation failed');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.writeFile(filePath, fileContent);
+    } else {
+      throw error;
+    }
+  }
+}
+
+createFile();
