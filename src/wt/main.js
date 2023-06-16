6e64ce7f3184e1cf4b4b5083826d49b1ee51e687
Syderi <syderi@yahoo.com>
2023-06-16 09:50:45 +0300

    feat: worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const workerPath = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+  const threads = os.cpus().length;
+  const promises = [];
+
+  for (let i = 0; i < threads; i++) {
+    const promise = new Promise((resolve) => {
+      const worker = new Worker(workerPath);
+      worker.on('message', (result) => {
+        resolve({ status: 'resolved', data: result });
+      });
+      worker.on('error', (error) => {
+        resolve({ status: 'error', data: null });
+      });
+      worker.postMessage(i + 10);
+    });
+    promises.push(promise);
+  }
+
+  const results = await Promise.all(promises);
+  return results;
 };
 
-await performCalculations();
+const printResults = async () => {
+  const results = await performCalculations();
+  console.log(results);
+};
+
+printResults();
