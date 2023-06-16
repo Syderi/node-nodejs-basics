b2e149e2773729300e7de4bd6163587184cfe9fd
Syderi <syderi@yahoo.com>
2023-06-16 09:33:28 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,31 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const scriptPath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn('node', [scriptPath, ...args], {
+    stdio: ['pipe', 'pipe', 'inherit'],
+  });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+
+  return new Promise((resolve, reject) => {
+    childProcess.on('close', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Child process exited with code ${code}`));
+      }
+    });
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2'])
+  .then(() => console.log('Child process completed'))
+  .catch((error) => console.error('Child process failed:', error));
