5c5e50b4cd2352eb5cfe05fa4c3385a554039dab
Syderi <syderi@yahoo.com>
2023-06-16 09:20:27 +0300

    feat: decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const filePathToGz = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(filePathToGz);
+    const gzip = zlib.createGzip();
+    readStream.pipe(gzip).pipe(writeStream);
+    await new Promise((resolve, reject) => {
+      writeStream.on('finish', resolve);
+      writeStream.on('error', reject);
+    });
+    console.log('Compression complete.');
+  } catch (error) {
+    console.error('Compression failed:', error);
+    throw Error('FS operation failed');
+  }
 };
 
 await compress();
